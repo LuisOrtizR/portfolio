daf03035d85f332329cf413ab861a624a8ddc370
Luis Ortiz <luisangel930115@gmail.com>
2026-02-21 09:11:47 -0500

    Initial portfolio setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="min-h-screen bg-neutral-950 text-neutral-100">
+    <RouterView />
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="min-h-screen flex items-center justify-center bg-black">
+    <h1 class="text-5xl font-bold text-white tracking-tight">
+      Luis Ortiz
+    </h1>
+  </section>
+</template>
